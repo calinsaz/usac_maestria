a5e157dd75a82b5a48c3c671abb662ab4d8e4ee9
admin <calinsaz@gmail.com>
2015-02-28 05:31:09 -0600

    alteracion de archivo 1

--- ClientEditor/src/clienteditor/RequiredStringValidator.java
+++ ClientEditor/src/clienteditor/RequiredStringValidator.java
@@ -39,7 +39,7 @@ import org.jdesktop.beansbinding.Validator;
 public class RequiredStringValidator extends Validator<String> {
 
     public Validator.Result validate(String arg) {
-        if ((arg == null) || (arg.length() == 0)) {
+        if ((arg == null) || (arg.length() == 0)) 
             return new Validator.Result(null, "Empty value");
         }
         return null;
